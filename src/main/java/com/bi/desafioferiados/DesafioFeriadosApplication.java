09d07e7d03b42e7cd2909ffe279c0a9e84856335
ManuelAlejandro20 <mtrigo@adom.cl>
2022-08-20 03:51:14 -0400

    feat: commit inicial

---
+++ src/main/java/com/bi/desafioferiados/DesafioFeriadosApplication.java
@@ -0,0 +1,13 @@
+package com.bi.desafioferiados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioFeriadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioFeriadosApplication.class, args);
+	}
+
+}
